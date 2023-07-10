a0d764b35ee5de02f9d72eda55b053df3bf30e65
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-07-10 18:26:21 +0530

    mini-added

---
+++ lua/hzln3t/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+    { 
+        "echasnovski/mini.nvim",
+        version = '*',
+        config = function()
+            require('mini.ai').setup()
+            require("mini.pairs").setup()
+            require("mini.surround").setup()
+        end
+    },
+}
