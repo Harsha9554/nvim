d94493bfbe13c0a36f9983f620f58c93569c29fd
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-07-10 18:29:26 +0530

    mini.comment-added

--- lua/hzln3t/plugins/mini.lua
+++ lua/hzln3t/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
         version = '*',
         config = function()
             require('mini.ai').setup()
+            require("mini.comment").setup()
             require("mini.pairs").setup()
             require("mini.surround").setup()
         end
