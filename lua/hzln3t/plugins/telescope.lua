ef792a5c59b540127696ad4d89b695faf2ba0c42
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-07-10 18:01:26 +0530

    telescope-browser-added

---
+++ lua/hzln3t/plugins/telescope.lua
@@ -0,0 +1,72 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim", 
+		tag = '0.1.2',
+		dependencies = { 
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-file-browser.nvim",
+        },
+        config = function()
+            local telescope = require("telescope")
+            local action_state = require("telescope.actions.state")
+            local actions = require("telescope.actions")
+
+            local function telescope_buffer_dir()
+                return vim.fn.expand("%:p:h")
+            end
+
+            actions.open_in_vscode = function(prompt_bufnr)
+                local path = action_state.get_selected_entry().value
+                local temp = string.format(":te code %s", path)
+                actions.close(prompt_bufnr)
+                vim.cmd(temp)
+            end
+
+            telescope.setup({
+                defaults = {
+                    prompt_prefix = " ",
+                    mappings = {
+                        n = {
+                            ["q"] = actions.close,
+                            ["v"] = actions.open_in_vscode,
+                        },
+                    },
+                    file_ignore_patterns = { "node_modules", ".git/" },
+                },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown",
+                        previewer = false,
+                        hidden = true,
+                        respect_gitignore = true,
+                    },
+                },
+                extensions = {
+                    file_browser = {
+                        dir_icon = " ",
+                        theme = "ivy",
+                        hijack_netrw = true,
+                        respect_gitignore = true,
+                        mappings = {},
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "<leader>e", function()
+                telescope.extensions.file_browser.file_browser({
+                    path = "%:p:h",
+                    cwd = telescope_buffer_dir(),
+                    respect_gitignore = false,
+                    hidden = true,
+                    grouped = true,
+                    previewer = false,
+                    initial_mode = "normal",
+                })
+            end)
+
+            telescope.load_extension("file_browser")
+
+        end
+	},
+
+}
