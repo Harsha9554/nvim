60e68d1f4e53f74a4a17efd4382551ab8eaf22d7
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-07-10 16:20:00 +0530

    treesitter-config-changes

--- lua/hzln3t/plugins/treesitter.lua
+++ lua/hzln3t/plugins/treesitter.lua
@@ -2,12 +2,11 @@ return {
     {
         "nvim-treesitter/nvim-treesitter", 
         build = ":TSUpdate",
-        config = {
+        opts = {
             ensure_installed = {
                 "bash",
                 "c",
                 "cmake",
-                "help",
                 "javascript",
                 "json",
                 "lua",
@@ -22,7 +21,7 @@ return {
                 "markdown_inline",
                 "html",
             }, -- one of "all" or a list of languages
-            ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+            context_commentstring = { enable = true }, 
             highlight = {
                 enable = true, -- false will disable the whole extension
                 disable = { "css" }, -- list of language that will be disabled
@@ -34,14 +33,12 @@ return {
             indent = { enable = true, disable = { "css" } },
             rainbow = {
                 enable = true,
-                -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-                max_file_lines = nil, -- Do not enable for files with more than n lines, int
-                -- colors = {}, -- table of hex strings
-                -- termcolors = {} -- table of colour name strings
+                query = "rainbow-parens",
             },
-
-        }
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
     },
 
     {
@@ -57,6 +54,4 @@ return {
             },
         }
     },
-
-	"JoosepAlviste/nvim-ts-context-commentstring",
 }
