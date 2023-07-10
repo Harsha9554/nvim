945defc14df3ba101295a304f6149ae195a0c189
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-07-10 18:38:11 +0530

    mini.starter-added

--- lua/hzln3t/plugins/mini.lua
+++ lua/hzln3t/plugins/mini.lua
@@ -3,10 +3,47 @@ return {
         "echasnovski/mini.nvim",
         version = '*',
         config = function()
-            require('mini.ai').setup()
+            require("mini.ai").setup()
             require("mini.comment").setup()
             require("mini.pairs").setup()
             require("mini.surround").setup()
+
+            local starter = require("mini.starter")
+            starter.setup({
+                evaluate_single = true,
+                footer = os.date(),
+                header = table.concat({
+                    [[           _                         _ ]],
+                    [[       _==/          i     i          \== ]],
+                    [[     /XX/            |\___/|            \XX\ ]],
+                    [[   /XXXX\            |XXXXX|            /XXXX\ ]],
+                    [[  |XXXXXX\_         _XXXXXXX_         _/XXXXXX| ]],
+                    [[ XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX ]],
+                    [[|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX| ]],
+                    [[XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ]],
+                    [[|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX| ]],
+                    [[ XXXXXX/^^^^"\XXXXXXXXXXXXXXXXXXXXX/^^^^^\XXXXXX ]],
+                    [[  |XXX|       \XXX/^^\XXXXX/^^\XXX/       |XXX| ]],
+                    [[    \XX\       \X/    \XXX/    \X/       /XX/ ]],
+                    [[       "\       "      \X/      "       /" ]],
+                    [[                        ! ]],
+                }, "\n"),
+                items = {
+                    function()
+                        return {
+                            { action = "Telescope find_files", name = "Files", section = "Telescope" },
+                            { action = "Telescope file_browser", name = "Browser", section = "Telescope" },
+                            { action = "Telescope live_grep", name = "Live Grep", section = "Telescope" },
+                        }
+                    end,
+                    starter.sections.builtin_actions(),               
+                },
+                content_hooks = {
+                    starter.gen_hook.adding_bullet(),
+                    starter.gen_hook.aligning("center", "center"),
+                    starter.gen_hook.padding(3, 2),
+                },           
+            })
         end
     },
 }
